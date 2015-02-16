eadb3f43054eafd7afaef67cf1223f720594321f
James <jamesghill@yahoo.co.uk>
2015-02-16 20:42:31 +0000

    Added a test for the 'getID' method
    Which failed as the 'getID' method has not yet been created.

--- test/ContactMangerTest/ContactImplTest.java
+++ test/ContactMangerTest/ContactImplTest.java
@@ -2,6 +2,7 @@ package ContactMangerTest;
 
 import contactmanager.*;
 import org.junit.*;
+import static org.junit.Assert.*;
 
 /**
  * Tests for the ContactImpl class.
@@ -18,6 +19,16 @@ public class ContactImplTest {
     @Before
     public void initialize(){
         newContact = new ContactImpl(1, "James Hill");
+    }
+    
+    /**
+     * Test the correct ID is returned.
+     */
+    @Test
+    public void testContactIDReturn(){
+        int checkID = 1;
+        int testID = newContact.getID();
+        assertEquals("The following incorrect ID has been returned: " + testID, checkID, testID);
     }
     
 }
