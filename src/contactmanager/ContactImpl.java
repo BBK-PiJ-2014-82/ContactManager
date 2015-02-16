c6b50e56fab8c0459e6d46cd04c080284579f257
James <jamesghill@yahoo.co.uk>
2015-02-16 20:02:26 +0000

    Moved into package 'contactmanager'

---
+++ src/contactmanager/ContactImpl.java
@@ -0,0 +1,37 @@
+package contactmanager;
+
+/**
+ * This class implements the 'Contact' interface.
+ * 
+ * @author James
+ */
+public class ContactImpl {
+    
+    /**
+     * This is a unique ID through which the application can identify a
+     * contact.
+     */
+    int contactID;
+    
+    /**
+     * This is the name given to the contact by the user.
+     */
+    String contactName;
+    
+    /**
+     * This is a string of notes that can record information about a contact.
+     */
+    String contactNotes;
+    
+    /**
+     * This is the class constructor.
+     * 
+     * @param contactID the unique ID is provided by the application. 
+     * @param contactName the name is provided by the user upon creation.
+     */
+    void ContactImpl(int contactID, String contactName){
+        this.contactID = contactID;
+        this.contactName = contactName;
+    }
+    
+}
