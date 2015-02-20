0a9062dc3536c4dc3e87c56b14ce76f6a5d6eb05
James <jamesghill@yahoo.co.uk>
2015-02-20 15:44:13 +0000

    Modified the class to include strict implementation

--- src/contactmanager/ContactImpl.java
+++ src/contactmanager/ContactImpl.java
@@ -5,7 +5,7 @@ package contactmanager;
  * 
  * @author James
  */
-public class ContactImpl {
+public class ContactImpl implements interfaces.Contact {
     
     /**
      * This is a unique ID through which the application can identify a
@@ -35,38 +35,22 @@ public class ContactImpl {
         this.contactNotes = "";
     }
     
-    /**
-     * Returns the unique contactID for this contact.
-     * 
-     * @return the contactID variable.
-     */
-    public int getID(){
+    @Override
+    public int getId(){
         return contactID;
     }
     
-    /**
-     * Returns the name for this contact.
-     * 
-     * @return the name recorded for this contact.
-     */
+    @Override
     public String getName(){
         return contactName;
     }
     
-    /**
-     * Returns the notes added to this contact or a blank if there are none.
-     * 
-     * @return the notes attached to this contact.
-     */
+    @Override
     public String getNotes(){
         return contactNotes;
     }
     
-    /**
-     * Adds the note entered by the user to the contact.
-     * 
-     * @param note the notes to be added.
-     */
+    @Override
     public void addNotes(String note){
         this.contactNotes = note;
     }
