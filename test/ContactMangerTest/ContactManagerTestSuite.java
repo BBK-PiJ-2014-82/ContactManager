a9fa514518d55a9a7f62b616d22027ef599a4ec6
James <jamesghill@yahoo.co.uk>
2015-02-21 06:02:19 +0000

    Added 'MeetingImplTest' to the suite.
    Also made minor changes to the formatting.

--- test/ContactMangerTest/ContactManagerTestSuite.java
+++ test/ContactMangerTest/ContactManagerTestSuite.java
@@ -4,7 +4,10 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ContactImplTest.class})
-public class ContactManagerTestSuite {
-    
-}
+@Suite.SuiteClasses(
+        {
+            ContactImplTest.class,
+            MeetingImplTest.class
+        })
+
+public class ContactManagerTestSuite {}
