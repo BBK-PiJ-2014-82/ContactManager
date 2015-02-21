67d645e06f541ae99f2d800e971fa30c23cfaaf6
James <jamesghill@yahoo.co.uk>
2015-02-21 03:09:28 +0000

    Created the variables and constructor

---
+++ src/contactmanager/MeetingImpl.java
@@ -0,0 +1,42 @@
+package contactmanager;
+
+import java.util.Calendar;
+import java.util.Set;
+
+/**
+ * This class implements the 'Meeting' interface.
+ *
+ * @author James
+ */
+public class MeetingImpl implements interfaces.Meeting {
+    
+    /**
+     * This is a unique ID through which the application can identify a
+     * meeting.
+     */
+    int meetingID;
+    
+    /**
+     * This is the date and time of the meeting stored as a Calendar object.
+     */
+    Calendar meetingDate;
+    
+    /**
+     * This is a set containing the Contacts who attended the meeting.
+     */
+    Set<ContactImpl> meetingContacts;
+    
+    /**
+     * This is the class constructor.
+     * 
+     * @param meetingID a unique ID for this meeting.
+     * @param meetingContacts a list of attendees for the meeting.
+     * @param meetingDate the date at which the meeting takes place.
+     */
+    public MeetingImpl(int meetingID, Set<ContactImpl> meetingContacts, Calendar meetingDate){
+        this.meetingID = meetingID;
+        this.meetingContacts = meetingContacts;
+        this.meetingDate = meetingDate;
+    }
+    
+}
