bd824f7bcd69f6a1301b36b2e1cec39fb9c7b47b
James <jamesghill@yahoo.co.uk>
2015-02-22 23:36:03 +0000

    Created the fields and constructor for this class.

---
+++ src/contactmanager/PastMeetingImpl.java
@@ -0,0 +1,30 @@
+package contactmanager;
+
+import interfaces.Contact;
+import java.util.Calendar;
+import java.util.Set;
+
+/**
+ * This is an implementation of the 'PastMeeting' class.
+ *
+ * @author James
+ */
+public class PastMeetingImpl extends MeetingImpl {
+    
+    /**
+     * This String is the notes for the meeting.
+     */
+    String meetingNotes;
+    
+    /**
+     * This is the class constructor.
+     * 
+     * @param meetingID a unique ID for this meeting.
+     * @param meetingContacts a list of attendees for the meeting.
+     * @param meetingDate the date at which the meeting takes place.
+     */
+    public PastMeetingImpl(int meetingID, Set<Contact> meetingContacts, Calendar meetingDate) {
+        super(meetingID, meetingContacts, meetingDate);
+    }
+    
+}
