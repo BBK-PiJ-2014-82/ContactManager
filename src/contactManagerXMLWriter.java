3b2e422e0a3cf8a1209d9af78dc169632a6f15f3
James <jamesghill@yahoo.co.uk>
2015-03-04 21:40:10 +0000

    Created interface for an XML writer class
    This is to create a class that will allow the print of XML data to a file.

---
+++ src/contactManagerXMLWriter.java
@@ -0,0 +1,26 @@
+import java.util.List;
+import java.util.Set;
+
+/**
+ * A class to output the contactManager data into XML format.
+ * 
+ * @author James
+ */
+public interface contactManagerXMLWriter {
+    
+    /**
+     * Builds a DOM document for this contact manager.
+     * 
+     * @param contacts the set of contacts to be saved.
+     * @param meetings the list of contact to be saved.
+     * @param nextContactID the next contactID that will be used.
+     * @param nextMeetingID the next meetingID that will be used.
+     * return the DOM structure constructed by this class.
+     */
+    public void build(Set<Contact> contacts, List<Meeting> meetings, int nextContactID, int nextMeetingID);
+    
+    /**
+     * Prints a DOM document to a file on the users system.
+     */
+    public void print();
+}
