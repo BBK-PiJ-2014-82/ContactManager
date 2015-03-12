e6b8cabd32f1eb2eff8dcde8b812e5c6e3bc7086
James <jamesghill@yahoo.co.uk>
2015-03-12 17:25:30 +0000

    Removed the constructor method as interfaces shouldn't have constructors.

--- src/Today.java
+++ src/Today.java
@@ -9,12 +9,6 @@ import java.util.Calendar;
 public interface Today {
 
     /**
-     * This is the class constructor.  It will create a new 'Today' object that
-     * is a wrapper for a Calendar initially set to the system date / time.
-     */
-    public void today();
-    
-    /**
      * This setter allows the tester of the software to set a date in order to
      * allow the testing of time sensitive methods in the ContactManagerImpl.
      * 
